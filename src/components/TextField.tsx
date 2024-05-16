10aad3a7d8c7d9f36b7d3c45685fa2cdf14f58ff
Santiago Morales <santiagomorales.reinoso@gmail.com>
2024-05-16 16:37:52 -0500

    *remove unused props

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -5,54 +5,36 @@ type TextFieldProps = {
   placeholder?: string;
   icon?: ReactNode;
   isSearch?: boolean;
-  multiline?: boolean;
-  errorMessage?: string;
 } & InputHTMLAttributes<HTMLInputElement> &
   InputHTMLAttributes<HTMLTextAreaElement>;
 
 const TextField = forwardRef<
   HTMLInputElement & HTMLTextAreaElement,
   TextFieldProps
->(
-  (
-    { label, placeholder, icon, isSearch, errorMessage, multiline, ...rest },
-    ref,
-  ) => {
-    return (
-      <div className={`flex flex-col ${label && "gap-4"}`}>
-        <p className="font-semibold">{label}</p>
-        {multiline ? (
-          <textarea
-            ref={ref}
-            placeholder={placeholder}
-            className="w-full rounded-md border p-2 outline-none"
-            {...rest}
-          />
-        ) : (
-          <div className={`${icon && "flex items-center gap-4 pl-2"}`}>
-            <div className="text-3xl">{icon}</div>
-            <div className="flex w-full flex-col gap-2">
-              <div
-                className={`flex rounded-lg border bg-white px-3 py-3 ${isSearch && "flex items-center justify-between gap-2"}`}
-              >
-                {isSearch && <IoIosSearch />}
-                <input
-                  ref={ref}
-                  className="w-full appearance-none bg-white leading-tight text-gray-700 focus:outline-none"
-                  placeholder={placeholder}
-                  {...rest}
-                  autoComplete={""}
-                />
-              </div>
-            </div>
+>(({ label, placeholder, icon, isSearch, ...rest }, ref) => {
+  return (
+    <div className={`flex flex-col ${label && "gap-4"}`}>
+      <p className="font-semibold">{label}</p>
+
+      <div className={`${icon && "flex items-center gap-4 pl-2"}`}>
+        <div className="text-3xl">{icon}</div>
+        <div className="flex w-full flex-col gap-2">
+          <div
+            className={`flex rounded-lg border bg-white px-3 py-3 ${isSearch && "flex items-center justify-between gap-2"}`}
+          >
+            {isSearch && <IoIosSearch />}
+            <input
+              ref={ref}
+              className="w-full appearance-none bg-white leading-tight text-gray-700 focus:outline-none"
+              placeholder={placeholder}
+              {...rest}
+              autoComplete={""}
+            />
           </div>
-        )}
-        {errorMessage && (
-          <div className="text-xs text-red-500">{errorMessage}</div>
-        )}
+        </div>
       </div>
-    );
-  },
-);
+    </div>
+  );
+});
 
 export default TextField;
